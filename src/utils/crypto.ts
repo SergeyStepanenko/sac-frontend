57b7ba86eda550dad4bcb93122e5856bbf017c6f
Sergey Stepanenko <sergey.stepanenko@hotmail.com>
2021-01-28 19:16:34 +0300

    first commit

---
+++ src/utils/crypto.ts
@@ -0,0 +1,13 @@
+import CryptoJS from 'crypto-js';
+
+const SECRET_KEY = 'My Secret Passphrase';
+
+export function encrypt(text: string): string {
+  return CryptoJS.AES.encrypt(text, SECRET_KEY).toString();
+}
+
+export function decrypt(text: string): string {
+  return CryptoJS.AES.decrypt(text, SECRET_KEY).toString(CryptoJS.enc.Utf8);
+}
+
+console.log(decrypt(encrypt('ololololo')));
